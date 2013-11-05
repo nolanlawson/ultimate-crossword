cab24539ca641c0fa5035d939b57dad355300263
Nolan Lawson <nolan.lawson@gmail.com>
2013-11-04 16:29:46 -0800

    performance improvements using batch file reading

--- create_db.py
+++ create_db.py
@@ -3,7 +3,7 @@
 
 import re, sqlite3
 
-BATCH_SIZE = 100000
+BATCH_SIZE = 500000
 TOTAL_NUM_LINES = 153004874
 
 conn = sqlite3.connect('cred.db')
@@ -17,15 +17,14 @@ create table users (
   email text, 
   password text, 
   hint text);''')
-conn.execute('create index users_id_idx on users (id);');
-conn.execute('create index users_email_idx on users(email);');
-conn.execute('create index users_password_idx on users(password);');
+
+#conn.execute('create index users_id_idx on users (id);');
+#conn.execute('create index users_email_idx on users(email);');
+#conn.execute('create index users_password_idx on users(password);');
 
 # batch process the file for better performance
-buffer = []
-
-def load_from_buffer():
-  conn.executemany('insert into users values (?,?,?,?,?);', buffer)
+def load_parsed_lines(lines):
+  conn.executemany('insert into users values (?,?,?,?,?);', lines)
 
 def parse_line(line):
   line = line.strip()
@@ -34,22 +33,19 @@ def parse_line(line):
   return line.split('-|-') 
 
 numlines = 0
-for line in open('cred','rb'):
-  line = parse_line(line)
-  
-  if len(line) < 5:
-    continue # empty line
-  elif len(line) > 5:
-    print "found a line with too many fields, ignoring extra fields:",line
-    line = line[:5]
-  buffer.append(line)
-  numlines += 1
-  
-  if len(buffer) == BATCH_SIZE:
-    load_from_buffer()
-    del buffer[:] # clear
-    print 'processed %d/%d (%.4f%%) lines from cred file so far...' % (numlines, TOTAL_NUM_LINES, (numlines * 1.0 / TOTAL_NUM_LINES))
+credfile = open('cred','rb')
+lines_batch = credfile.readlines(BATCH_SIZE)
 
-if len(buffer) > 0:
-  load_from_buffer()
-  
+while lines_batch:
+  parsed_lines = [parse_line(line) for line in lines_batch]
+
+  parsed_lines = filter((lambda line: len(line) >= 5), parsed_lines) # empty lines
+
+  for line in parsed_lines:
+    if len(line) > 5:
+      print "found a line with too many fields, ignoring extra fields:",line
+      line = line[:5]
+
+  load_parsed_lines(parsed_lines)
+  numlines += len(parsed_lines)
+  print 'processed %d/%d (%.4f%%) lines from cred file so far...' % (numlines, TOTAL_NUM_LINES, (numlines * 1.0 / TOTAL_NUM_LINES))
