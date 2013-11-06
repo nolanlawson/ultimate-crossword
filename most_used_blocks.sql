744371ea5f08b4bdeed13d5c99100b71fbbb4d43
Nolan Lawson <nolan.lawson@gmail.com>
2013-11-05 18:35:57 -0800

    add sql script

---
+++ most_used_blocks.sql
@@ -0,0 +1,10 @@
+
+SELECT x.*,group_concat(u.hint) from
+(select count(*) as counter, bu.block_id 
+FROM user_blocks bu where bu.block_location = 0 
+group by bu.block_id having counter > 10 order by counter desc
+limit 200) x
+join user_blocks ub on x.block_id = ub.block_id and ub.block_location = 0
+join users u on ub.user_id = u.id
+where u.hint != '' and u.hint is not null
+group by x.block_id order by counter desc
