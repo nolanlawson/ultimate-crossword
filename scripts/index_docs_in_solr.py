a95e55f30c9486b2c28ddf873d1355d18a7f584d
Nolan Lawson <nolan.lawson@gmail.com>
2013-11-09 20:28:34 -0800

    modify solr script

--- scripts/index_docs_in_solr.py
+++ scripts/index_docs_in_solr.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 #
 # Read docs from the optimized CouchDB, load them in Solr
+# Deletes all documents in Solr before adding them!
 #
 import requests, json
 
@@ -10,19 +11,34 @@ SOLR_URL = 'http://localhost:8983/solr'
 
 COUCHDB_BULK_SIZE = 10000
 
+print "Deleting all docs in Solr"
+delete_response = requests.post(SOLR_URL + '/update/json', params={'commit' : 'true'},\
+    headers={'Content-Type' : 'application/json'},data=json.dumps({'delete' : {'query' : '*:*'}}))
+print "Response was",delete_response.status_code
+
 for (couchdb_url, doc_type) in zip((COUCHDB_SUMMARIES_URL, COUCHDB_RELATED_URL), ('summary', 'related')):
-  params = {limit : COUCHDB_BULK_SIZE}
+  params = {'limit' : COUCHDB_BULK_SIZE,'include_docs' : 'true'}
   while True:
     response = requests.get(couchdb_url + '/_all_docs', params=params)
-    print "called couch, got response code ", response.status_code
+    print "called couch %s, got response code %d" % (couchdb_url, response.status_code)
     rows = response.json()['rows']
+    
+    # filter design documents and hintless documents
+    rows = filter(lambda row : ('hints' in row['doc']) and (len(row['doc']['hints']) > 0), rows)
+    
+    if len(rows) == 0:
+      break
+    
+    solr_docs = map(lambda row : {'id' : row['doc']['_id'], 'hints' : row['doc']['hints'], 'docType' : doc_type}, rows)
+  
+    solr_response = requests.post(SOLR_URL + '/update/json', params={'commit' : 'true'},\
+        headers={'Content-Type' : 'application/json'},data=json.dumps({'add' : solr_docs}))
+    
+    print "Posted %d docs to solr, response was %d" % (len(solr_docs), solr_response.status_code)
+    print solr_response.text
     
     if len(rows) < COUCHDB_BULK_SIZE: # done reading from couchdb
       break
-      
-    solr_docs = map(lambda row : {'id' : row['_id'], 'hints' : row['hints'], 'docType' : doc_type}, rows)
-  
-    requests.post(SOLR_URL + '/update/json', params={'commit' : 'true'},headers={'Content-Type' : 'application/json'},data=json.dumps(solr_docs))
-  
+    
     last_row = rows[-1]
-    params.update({'startkey'  : last_row['_id'], 'skip' : 1})
+    params.update({'startkey'  : json.dumps(last_row['doc']['_id']), 'skip' : 1})
