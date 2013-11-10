d9338af77e01d5968feea21e5865cffd0226e03e
Nolan Lawson <nolan.lawson@gmail.com>
2013-11-10 11:11:33 -0800

    add counts to solr

--- scripts/index_docs_in_solr.py
+++ scripts/index_docs_in_solr.py
@@ -17,19 +17,29 @@ delete_response = requests.post(SOLR_URL + '/update/json', params={'commit' : 't
 print "Response was",delete_response.status_code
 
 for (couchdb_url, doc_type) in zip((COUCHDB_SUMMARIES_URL, COUCHDB_RELATED_URL), ('summary', 'related')):
+  
+  def couch_row_to_solr_doc(row):
+    doc = row['doc']
+    return {\
+        'id' : doc['_id'],\
+        'hints' : doc['hints'],\
+        'docType' : doc_type,\
+        'popularity' : doc['count'] if doc_type == 'related' else (doc['soloCount'] + doc['followingCount'])}
+  
   params = {'limit' : COUCHDB_BULK_SIZE,'include_docs' : 'true'}
+  
   while True:
     response = requests.get(couchdb_url + '/_all_docs', params=params)
     print "called couch %s, got response code %d" % (couchdb_url, response.status_code)
     rows = response.json()['rows']
     
-    # filter design documents and hintless documents
-    rows = filter(lambda row : ('hints' in row['doc']) and (len(row['doc']['hints']) > 0), rows)
-    
     if len(rows) == 0:
       break
+          
+    # filter design documents and hintless documents
+    filtered_rows = filter(lambda row : ('hints' in row['doc']) and (len(row['doc']['hints']) > 0), rows)
     
-    solr_docs = map(lambda row : {'id' : row['doc']['_id'], 'hints' : row['doc']['hints'], 'docType' : doc_type}, rows)
+    solr_docs = map(couch_row_to_solr_doc, filtered_rows)
   
     solr_response = requests.post(SOLR_URL + '/update/json', params={'commit' : 'true'},\
         headers={'Content-Type' : 'application/json'},data=json.dumps({'add' : solr_docs}))
