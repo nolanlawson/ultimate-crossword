29361939b9e11815c7a884c94bae3ecebf432cf5
Nolan Lawson <nolan.lawson@gmail.com>
2013-11-10 14:36:20 -0800

    put hints in separate db

--- scripts/compute_optimized_couchdb.py
+++ scripts/compute_optimized_couchdb.py
@@ -10,14 +10,15 @@
 import requests, json, sys, base64, re, itertools
 
 COUCHDB_BULK_INSERT_SIZE = 100
+MAX_NUM_HINTS_IN_SUMMARY = 30
 
 # if true, sets stale to update_after
 DEBUG_MODE = False
 
 INPUT_URL = 'http://localhost:5984/blocks'
-OUTPUT_URL = 'http://localhost:5984/block_summaries2'
-OUTPUT_DETAILS_URL = 'http://localhost:5984/related_blocks2'
-
+OUTPUT_URL = 'http://localhost:5984/block_summaries'
+OUTPUT_DETAILS_URL = 'http://localhost:5984/related_blocks'
+OUTPUT_HINTS_URL = 'http://localhost:5984/block_hints'
 
 design_documents = [
 {
@@ -65,9 +66,9 @@ def create_block_document(block, count):
     (key, hints) = (block_hint['key'], block_hint['doc']['hints'])
     
     if len(key[0]) > 1: # has a related block
-      (related_block, reverse_order) = (key[0][1], key[1])
+      (related_block, reverse_keys_and_values) = (key[0][1], key[1])
       
-      key = 'precedingBlocks' if reverse_order else 'followingBlocks';
+      key = 'precedingBlocks' if reverse_keys_and_values else 'followingBlocks';
       try:
         result[key][anonymize(related_block)] += hints
       except KeyError:
@@ -76,10 +77,38 @@ def create_block_document(block, count):
       result['hints'] += hints
   return result
   
+  
+def create_hint_map(hints):
+  result = {}
+  for hint in hints:
+    try:
+      result[hint] += 1
+    except KeyError:
+      result[hint] = 1
+  return result
+
+def reverse_keys_and_values((k,v)):
+  return (v,k)
+
+def add_doc_hints_if_necessary(doc, doc_hints):
+  # put the hints in a separate db, because they take up too much room
+  # also, model the hints as a map of strings to ints rather than a list
+  hint_map = create_hint_map(doc['hints'])
+  if (len(hint_map.keys()) > MAX_NUM_HINTS_IN_SUMMARY):
+    sorted_hint_map = map(reverse_keys_and_values, hint_map.items());
+    sorted_hint_map.reverse()
+    top_hints = dict(map(reverse_keys_and_values, sorted_hint_map[:MAX_NUM_HINTS_IN_SUMMARY]))
+    doc['hintMap'] = top_hints
+    doc['hintsRedacted'] = True
+    doc_hints.append({'_id' : doc['_id'], 'hintMap' : hint_map})
+  else:
+    doc['hintMap'] = hint_map
+  
+  del doc['hints']
 
 def split_doc_into_summary_and_details(doc):
   # split docs into an optimized summary/detail format and post
-  # to two separate databases
+  # to three separate databases
   
   # create separate docs
   related_blocks = []
@@ -99,26 +128,30 @@ def split_doc_into_summary_and_details(doc):
   doc['soloCount'] = len(doc['hints'])
   doc['followingCount'] = len(doc['followingBlocks'])
   doc['precedingCount'] = len(doc['precedingBlocks'])
-  
   # remove details from original doc
   del doc['precedingBlocks']
   del doc['followingBlocks']
   
+  # put the hints in an entirely separate database, because they take up too much room  
+  doc_hints = []
+  add_doc_hints_if_necessary(doc, doc_hints)
+    
   # apply an id to look like this : <blockId>~01, <blockId>~02
   # this means we can sort lexicographically in CouchDB by blockId, then number of hints (descending)
   max_id_len = len(str(len(related_blocks)))
   for i in range(len(related_blocks)):
     related_block = related_blocks[i]
     related_block['_id'] = doc['_id'] + "~" + str(i).zfill(max_id_len)
-  
-  return (doc, related_blocks)
+    add_doc_hints_if_necessary(related_block, doc_hints)
+    
+  return (doc, related_blocks, doc_hints)
 
 def post_bulk(url, docs):
   
   response = requests.post(url + '/_bulk_docs',data=json.dumps({'docs' : docs}),headers={'Content-Type':'application/json'})
-  print "Posted %d documents to %s, response: %d" % (len(docs), url, response.status_code)
+  print " > Posted %d documents to %s, response: %d" % (len(docs), url, response.status_code)
   if (str(response.status_code).startswith('4')): # error
-    print " > Got error", response.json()
+    print " > > Got error", response.json()
   
 def post_documents_to_couchdb(docs, last_counter):
   
@@ -126,12 +159,18 @@ def post_documents_to_couchdb(docs, last_counter):
   
   summary_docs = map(lambda x : x[0], summaries_and_details)
   details_docs = map(lambda x : x[1], summaries_and_details)
+  hints_docs = map(lambda x : x[2], summaries_and_details)
   details_docs = list(itertools.chain(*details_docs)) # flatten
-    
+  hints_docs = list(itertools.chain(*hints_docs)) # flatten
+  
+  
+  print "Posting %d docs..." % len(summary_docs);
+  
   post_bulk(OUTPUT_URL, summary_docs)
   post_bulk(OUTPUT_DETAILS_URL, details_docs)
+  post_bulk(OUTPUT_HINTS_URL, hints_docs)
   
-  print "Posted %d blocks total..." % (last_counter)
+  print " > Posted %d blocks total." % (last_counter)
   
 def create_block_documents():
   
@@ -147,6 +186,7 @@ def create_block_documents():
     if (len(block_counts) == 0):
       break
     
+    print "Received %d docs from CouchDB" % len(block_counts['rows'])
     docs_batch = map((lambda row : create_block_document(row['key'], row['value'])), block_counts['rows'])
     
     counter += len(docs_batch)
@@ -161,7 +201,7 @@ def create_block_documents():
 def main():
   
   # drop and re-create both output databases
-  for url in (OUTPUT_URL, OUTPUT_DETAILS_URL):
+  for url in (OUTPUT_URL, OUTPUT_DETAILS_URL, OUTPUT_HINTS_URL):
     print 'dropping database %s, response is %s' % (url, requests.delete(url).status_code)
     print 'creating database %s, response is %s' % (url, requests.put(url).status_code)
   
--- scripts/index_docs_in_solr.py
+++ scripts/index_docs_in_solr.py
@@ -5,50 +5,82 @@
 #
 import requests, json
 
-COUCHDB_SUMMARIES_URL = 'http://localhost:5984/block_summaries2'
-COUCHDB_RELATED_URL = 'http://localhost:5984/related_blocks2'
+COUCHDB_SUMMARIES_URL = 'http://localhost:5984/block_summaries'
+COUCHDB_RELATED_URL = 'http://localhost:5984/related_blocks'
+COUCHDB_HINTS_URL = 'http://localhost:5984/block_hints'
+
 SOLR_URL = 'http://localhost:8983/solr'
 
 COUCHDB_BULK_SIZE = 10000
 
-print "Deleting all docs in Solr"
-delete_response = requests.post(SOLR_URL + '/update/json', params={'commit' : 'true'},\
-    headers={'Content-Type' : 'application/json'},data=json.dumps({'delete' : {'query' : '*:*'}}))
-print "Response was",delete_response.status_code
+def enhance_with_full_hints(rows):
+  # fetch hints from the block_hints database if necessary
+  keys = map(lambda row : row['doc']['_id'], filter((lambda row : 'hintsRedacted' in row['doc'] and row['doc']['hintsRedacted']),rows))
+  
+  if (len(keys) == 0): # no hints redacted
+    return rows;
+  
+  
+  response = requests.get(COUCHDB_HINTS_URL + '/_all_docs', params={'keys' : json.dumps(keys), 'include_docs' : 'true'}).json();
+  
+  ids_to_hints = dict(map(lambda row : (row['key'], row['doc']['hintMap']), response['rows']))
+  
+  for row in rows:
+    if 'hintsRedacted' in row['doc'] and row['doc']['hintsRedacted']:
+      row['doc']['hintMap'] = ids_to_hints[row['doc']['_id']]
 
-for (couchdb_url, doc_type) in zip((COUCHDB_SUMMARIES_URL, COUCHDB_RELATED_URL), ('summary', 'related')):
   
-  def couch_row_to_solr_doc(row):
-    doc = row['doc']
-    return {\
-        'id' : doc['_id'],\
-        'hints' : doc['hints'],\
-        'docType' : doc_type,\
-        'popularity' : doc['count'] if doc_type == 'related' else (doc['soloCount'] + doc['followingCount'])}
+def main():
   
-  params = {'limit' : COUCHDB_BULK_SIZE,'include_docs' : 'true'}
+  print "Deleting all docs in Solr"
+  delete_response = requests.post(SOLR_URL + '/update/json', params={'commit' : 'true'},\
+      headers={'Content-Type' : 'application/json'},data=json.dumps({'delete' : {'query' : '*:*'}}))
+  print "Response was",delete_response.status_code
+
+  for (couchdb_url, doc_type) in zip((COUCHDB_SUMMARIES_URL, COUCHDB_RELATED_URL), ('summary', 'related')):
+    params = {'limit' : COUCHDB_BULK_SIZE,'include_docs' : 'true'}
   
-  while True:
-    response = requests.get(couchdb_url + '/_all_docs', params=params)
-    print "called couch %s, got response code %d" % (couchdb_url, response.status_code)
-    rows = response.json()['rows']
+    def couch_row_to_solr_doc(row):
+      doc = row['doc']
+
+      # repeat the repeated hints so solr can weight them properly
+      hints = []
+      for (hint, count) in doc['hintMap'].items():
+        for i in range(count):
+          hints.append(hint)
+
+      return {\
+          'id' : doc['_id'],\
+          'hints' : hints,\
+          'docType' : doc_type,\
+          'popularity' : doc['count'] if doc_type == 'related' else (doc['soloCount'] + doc['followingCount'])}  
+  
+    while True:
+      response = requests.get(couchdb_url + '/_all_docs', params=params)
+      print "called couch %s, got response code %d" % (couchdb_url, response.status_code)
+      rows = response.json()['rows']
     
-    if len(rows) == 0:
-      break
+      if len(rows) == 0:
+        break
           
-    # filter design documents and hintless documents
-    filtered_rows = filter(lambda row : ('hints' in row['doc']) and (len(row['doc']['hints']) > 0), rows)
+      # filter design documents and hintless documents
+      filtered_rows = filter(lambda row : ('hintMap' in row['doc']) and (len(row['doc']['hintMap'].keys()) > 0), rows)
     
-    solr_docs = map(couch_row_to_solr_doc, filtered_rows)
+      enhanced_rows = enhance_with_full_hints(filtered_rows)
+    
+      solr_docs = map(couch_row_to_solr_doc, filtered_rows)
   
-    solr_response = requests.post(SOLR_URL + '/update/json', params={'commit' : 'true'},\
-        headers={'Content-Type' : 'application/json'},data=json.dumps({'add' : solr_docs}))
+      solr_response = requests.post(SOLR_URL + '/update/json', params={'commit' : 'true'},\
+          headers={'Content-Type' : 'application/json'},data=json.dumps({'add' : solr_docs}))
     
-    print "Posted %d docs to solr, response was %d" % (len(solr_docs), solr_response.status_code)
-    print solr_response.text
+      print "Posted %d docs to solr, response was %d" % (len(solr_docs), solr_response.status_code)
+      print solr_response.text
     
-    if len(rows) < COUCHDB_BULK_SIZE: # done reading from couchdb
-      break
+      if len(rows) < COUCHDB_BULK_SIZE: # done reading from couchdb
+        break
     
-    last_row = rows[-1]
-    params.update({'startkey'  : json.dumps(last_row['doc']['_id']), 'skip' : 1})
+      last_row = rows[-1]
+      params.update({'startkey'  : json.dumps(last_row['doc']['_id']), 'skip' : 1})
+
+if __name__=='__main__':
+  main()
